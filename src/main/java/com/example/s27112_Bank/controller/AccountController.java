dcb12a1c7edf03ce1ea6027ffdf4c6dc36f259f0
s27112-pj <39103506+KacperPach@users.noreply.github.com>
2024-01-28 14:46:36 +0100

    last

--- src/main/java/com/example/s27112_Bank/controller/AccountController.java
+++ src/main/java/com/example/s27112_Bank/controller/AccountController.java
@@ -1,10 +1,14 @@
 package com.example.s27112_Bank.controller;
 
 import com.example.s27112_Bank.model.account.Account;
+import com.example.s27112_Bank.model.account.components.AccountCurrency;
 import com.example.s27112_Bank.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Currency;
+import java.util.List;
 
 @RestController
 @RequestMapping("/account")
@@ -27,6 +31,12 @@ public class AccountController {
     public ResponseEntity<Account> createAccount( @RequestBody Account account) {
         Account createdAccount = accountService.create(account);
         return ResponseEntity.ok(createdAccount);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Account>> getAccountsByParam(@RequestParam(required = false) Double val, AccountCurrency cur) {
+        List<Account> account = accountService.getAccountsByMinimalBalance(val,cur);
+        return ResponseEntity.ok(account);
     }
 
 }
