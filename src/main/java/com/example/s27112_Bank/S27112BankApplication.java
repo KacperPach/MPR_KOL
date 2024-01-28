40ec52ea58806a2d09763ed16ce973a4f23c402a
s27112-pj <39103506+KacperPach@users.noreply.github.com>
2024-01-28 12:09:09 +0100

    initial commit

---
+++ src/main/java/com/example/s27112_Bank/S27112BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s27112_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S27112BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S27112BankApplication.class, args);
+	}
+
+}
